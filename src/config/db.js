19111effb8c55889bee7f36db339762b7a2ce62a
anisulhaqueniloy <niloy2931@gmail.com>
2026-01-16 06:23:08 +0600

    db has been configed

--- src/config/db.js
+++ src/config/db.js
@@ -13,10 +13,12 @@ const connectDB = async () => {
 
 module.exports = connectDB;
 
-
 /* 
 MONGO_URI =mongodb+srv://wtapphub:d0UFJ4yca1BirNAp@cluster0.3zxuw.mongodb.net/wtapphubDB?retryWrites=true&w=majority&appName=Cluster0
 JWT_SECRET=wtapphub_super_secret_key_12345
 JWT_EXPIRES_IN=7d
+
+WASENDER_TOKEN = "82940f9e9fd07461bd9f6d0022e86c595e0e0968a5ff59dda55fc4809e3b5db3"
+WASENDER_URL = https://www.wasenderapi.com/api/send-message
 
 */
